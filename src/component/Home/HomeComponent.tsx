ff722eaefcf60416958d5e2e459b7f5279b4bd33
officialanurag <anuragg569@gmail.com>
2020-04-21 21:53:15 +0530

    design changes

--- src/component/Home/HomeComponent.tsx
+++ src/component/Home/HomeComponent.tsx
@@ -6,22 +6,21 @@ import HomemaintextComponent from '../Homemaintext/HomemaintextComponent';
 
 export const HomeComponent: React.FC = () => {
   return (
-          <div className={styles.homeMain}>
-            <div className={styles.overlay}>
-              <Container >
-                <div className = {styles.inner}>
-                  <Row>
-                    <Col xs={12} md={8} lg={8}  className='d-none d-lg-block'>
-                      <HomemaintextComponent/>
-                    </Col>  
-                    <Col xs={12} md={4} lg={4}>
-                      <SignupComponent/>
-                    </Col>
-                  </Row> 
-                </div>
-              </Container>
-            </div>
+    <div className={styles.homeMain}>
+      <div className={styles.overlay}>
+        <Container >
+          <div className={styles.inner}>
+            <Row>
+              <Col xs={12} md={8} lg={8} className='d-none d-lg-block'>
+                <HomemaintextComponent />
+              </Col>
+              <Col xs={12} md={4} lg={4}>
+                <SignupComponent />
+              </Col>
+            </Row>
           </div>
-        );
-      }
-
+        </Container>
+      </div>
+    </div>
+  );
+}
--- src/component/Homemaintext/HomemaintextComponent.tsx
+++ src/component/Homemaintext/HomemaintextComponent.tsx
@@ -4,37 +4,37 @@ import CheckIcon from '@material-ui/icons/Check';
 
 const HomemaintextComponent: React.FC<{}> = () => {
   return (
-          <div>
-             <h1 className="display-4">Build
-                <strong>social profiles</strong> and gain revenue
-                <strong>profits</strong>
-            </h1>
-            <div className="d-flex">
-              <div className="p-4 align-self-start">
-                <CheckIcon className={styles.checkicon}/>
+    <div>
+      <h1 className="display-4">Build
+        <strong>social profiles</strong> and gain revenue
+        <strong>profits</strong>
+      </h1>
+      <div className="d-flex">
+        <div className="p-4 align-self-start">
+          <CheckIcon className={styles.checkicon} />
+        </div>
+        <div className="p-4 align-self-end">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
               </div>
-              <div className="p-4 align-self-end">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
+      </div>
+      <div className="d-flex">
+        <div className="p-4 align-self-start">
+          <CheckIcon className={styles.checkicon} />
+        </div>
+        <div className="p-4 align-self-end">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
               </div>
-            </div>
-            <div className="d-flex">
-              <div className="p-4 align-self-start">
-              <CheckIcon className={styles.checkicon}/>
+      </div>
+      <div className="d-flex">
+        <div className="p-4 align-self-start">
+          <CheckIcon className={styles.checkicon} />
+        </div>
+        <div className="p-4 align-self-end">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
               </div>
-              <div className="p-4 align-self-end">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
-              </div>
-            </div>
-            <div className="d-flex">
-              <div className="p-4 align-self-start">
-               <CheckIcon className={styles.checkicon}/>
-              </div>
-              <div className="p-4 align-self-end">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
-              </div>
-            </div>
-          </div>
-        );
-      }
+      </div>
+    </div>
+  );
+}
 
-      export default HomemaintextComponent;
+export default HomemaintextComponent;
